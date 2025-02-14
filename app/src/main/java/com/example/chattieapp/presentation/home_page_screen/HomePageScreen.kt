1f65542dbf560dc8c727186f6f4dfc12f7adba9d
gultencelikbilek <81778970+gultencelikbilek@users.noreply.github.com>
2025-02-14 15:40:49 +0300

    firebase realtime database added

--- app/src/main/java/com/example/chattieapp/presentation/home_page_screen/HomePageScreen.kt
+++ app/src/main/java/com/example/chattieapp/presentation/home_page_screen/HomePageScreen.kt
@@ -1,31 +1,133 @@
 package com.example.chattieapp.presentation.home_page_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomePageScreen(navController: NavController) {
+fun HomePageScreen(
+    navController: NavController,
+    homePageViewModel: HomePageViewModel = hiltViewModel()
+) {
 
+    val channelState = homePageViewModel.channel.collectAsState()
+    var addChannel by remember {
+        mutableStateOf(false)
+    }
+    val sheetState = rememberModalBottomSheetState()
     Scaffold(
-        content = {paddingValues ->
+        floatingActionButton = {
             Box(modifier = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Blue)
+                .clickable {
+                    addChannel = true
+                }){
+                Text(text = "Add Channel", modifier = Modifier.padding(16.dp), color = White)
+            }
+        }) { paddingValues ->
+        Box(
+            modifier = Modifier
                 .padding(paddingValues)
-                .fillMaxSize()){
-                LazyColumn {
-                    item {
-                        Column {
-
-                        }
+                .clip(RoundedCornerShape(25.dp))
+                .fillMaxSize()
+        ) {
+            LazyColumn {
+                items(channelState.value) { channel ->
+                    Column {
+                        Text(
+                            text = channel.name,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(Color.Red.copy(alpha = 0.3f))
+                                .clickable {
+                                    // navController.navigate("chat/${channel.id}")
+                                }
+                                .padding(16.dp)
+                        )
                     }
                 }
             }
         }
-    )
+        if (addChannel) {
+            ModalBottomSheet(
+                onDismissRequest = { addChannel = false },
+                sheetState = sheetState
+            ) {
+                AddChannelDialog {
+                    homePageViewModel.addChannel(it)
+                    addChannel = false
+                }
+
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun AddChannelDialog(onAddChannel: (String) -> Unit) {
+    var channelName by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Add Channel")
+        Spacer(modifier = Modifier.padding(8.dp))
+        TextField(
+            value = channelName, onValueChange = {
+                channelName = it
+            },
+            label = {
+                Text(text = "Channel Name")
+            },
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Button(onClick = { onAddChannel(channelName) }, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Add")
+
+        }
+    }
 }
--- app/src/main/java/com/example/chattieapp/presentation/home_page_screen/HomePageViewModel.kt
+++ app/src/main/java/com/example/chattieapp/presentation/home_page_screen/HomePageViewModel.kt
@@ -1,12 +1,46 @@
 package com.example.chattieapp.presentation.home_page_screen
 
 import androidx.lifecycle.ViewModel
+import com.example.chattieapp.domain.model.Channel
 import com.google.firebase.Firebase
+import com.google.firebase.database.database
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomePageViewModel @Inject constructor() : ViewModel() {
 
-   // val firebaseDatabase = Firebase.
+    //Realtime database erişim sağlandı.
+    val firebaseDatabase = Firebase.database
+    private val _channels = MutableStateFlow<List<Channel>>(emptyList())
+    val channel = _channels.asStateFlow()
+
+    init {
+        getChannels()
+    }
+    private fun getChannels(){
+        //channel database erişim sağlanıyor.
+        // .get() çağrısı ile bu koleksiyondaki tüm veriler alınır.
+        //addOnSuccessListener ile veriler başarıyla alındığında yapılacak işlemleri tanımlıyorsunuz.
+        firebaseDatabase.getReference("channel").get().addOnSuccessListener {
+            val list = mutableListOf<Channel>()
+            it.children.forEach {data->
+                //data burada, Firebase'den gelen veriyi temsil ediyor.
+                //.children ile bu veri nesnesinin alt öğelerine erişiyorsunuz.
+                // Bu genellikle veritabanındaki her bir çocuk öğesini (yani her bir kanal verisini) temsil eder.
+                val channel = Channel(data.key!!, data.value!!.toString())
+                list.add(channel)
+            }
+            _channels.value = list
+        }
+    }
+
+    fun addChannel(name:String){
+        val key = firebaseDatabase.getReference("channel").push().key
+        firebaseDatabase.getReference("channel").child(key!!).setValue(name).addOnSuccessListener {
+            getChannels()
+        }
+    }
 }
